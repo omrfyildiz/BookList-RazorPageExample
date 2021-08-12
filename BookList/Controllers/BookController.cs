f0d489c4e3b3b0b043bc4d2fa117d73bcda06afd
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-08-12 18:45:03 +0300

    Controller page has been created for api

---
+++ BookList/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.Book.ToList() });
+        }
+    }
+}
